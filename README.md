bc7def9491ed54189ca826f4689a77971e4fba33
fanxiaoye <1024956214@qq.com>
2020-01-08 16:21:42 +0800

    访问静态页面 git commit -m 访问静态页面

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tornadoProject
 编写顺序是按照后面的数字进行记录的
+
+访问静态文件，这个路由写在最后面
+r'/(.*)$', tornado.web.StaticFileHandler, {'path': os.path.join(config.settings['static_path'], 'html'),
+                                                        'default_filename': 'index.html'})
--- muban02/application.py
+++ muban02/application.py
@@ -1,6 +1,7 @@
 #！/usr/bin/env python
 # _*_ coding:utf-8 _*_
 import tornado.web
+import os
 from views import index
 import config
 
@@ -8,7 +9,7 @@ import config
 class Application(tornado.web.Application):
     def __init__(self):
         handler = [
-            (r'/', index.httpHandler),
+            # (r'/', index.httpHandler),
 
             # request对象
             ('/requestObject', index.RequestObject),
@@ -24,5 +25,15 @@ class Application(tornado.web.Application):
 
             # 继承
             (r'/jicheng', index.JiChenHandler),
+
+
+
+
+
+
+
+            # 静态文件
+            (r'/(.*)$', tornado.web.StaticFileHandler, {'path': os.path.join(config.settings['static_path'], 'html'),
+                                                        'default_filename': 'index.html'})
         ]
         super(Application, self).__init__(handler, **config.settings)
